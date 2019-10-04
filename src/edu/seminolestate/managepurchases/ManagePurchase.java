53e1910a8b776229e7977adcbdfd8447e47bf0dc
5921001744 <5921001744@SHS-13-221-11.inst.scpsnet.edu>
2019-10-04 09:54:09 -0400

    .

--- src/edu/seminolestate/managepurchases/ManagePurchase.java
+++ src/edu/seminolestate/managepurchases/ManagePurchase.java
@@ -1,5 +1,8 @@
 package edu.seminolestate.managepurchases;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -12,20 +15,61 @@ public class ManagePurchase {
 	}
 	
 	public static void main(String[] args) {
+		displayMenu();
 		ArrayList<Purchase> list = new ArrayList<Purchase>();
-		int input;
-		
-		try {
-			Scanner sc = new Scanner(System.in);
-			input = sc.nextInt();
-			
-			if(input == 1) {
-				System.out.println("Enter ")
+		int input = 0;
+		int dateInt = 0;
+		Scanner sc = new Scanner(System.in);
+		while(input == 0) {
+			try {
+				System.out.println("Enter a number");
+				input = sc.nextInt();
+				 
+				
+			} catch (InputMismatchException e) {
+				System.out.println("Error: Please input a number");
+				input = 0;
+				displayMenu();
 			}
-			
-		} catch (InputMismatchException e) {
-			System.out.println("Error: Please input a number");
-			displayMenu();
+		}
+
+		while(input == 1) {
+			try {
+				System.out.println("Enter a product name");
+				String productName = sc.next();
+				if(productName.length() < 1) {
+					throw new Exception();
+				}
+				System.out.println("Enter a store name");
+				String storeName = sc.next();
+				if(storeName.length() < 1) {
+					throw new Exception();
+				}
+				while(dateInt == 0) {
+					try {
+						System.out.println("Enter a purchase date (like Jun 23, 2015)");
+						String purchaseDate = sc.next();
+						DateTimeFormatter formatter =
+								 DateTimeFormatter.ofPattern("MMM d, yyyy");
+						LocalDate date = LocalDate.parse(purchaseDate, formatter);
+					} catch (DateTimeParseException e) {
+						System.out.println("Enter a valid date");
+						dateInt = 0;
+					}
+				}
+				
+				System.out.println("Enter a cost");
+				double cost = sc.nextDouble();
+				if(cost < 0) {
+					throw new Exception();
+				}
+				Purchase purchase = new Purchase(productName, storeName, date, cost);
+				list.add(purchase);
+				
+			} catch (Exception e) {
+				System.out.println("Error: please enter valid data");
+				input = 1;
+			}	
 		}
 	}
 }
